0cb826a43f0e1ce7dd3966ea10dee6fbd6e22e8e
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-05-28 12:00:27 +0530

    create banners

--- frontend/src/Components/NewsLatter/NewsLatter.jsx
+++ frontend/src/Components/NewsLatter/NewsLatter.jsx
@@ -3,7 +3,7 @@ import './NewsLatter.css'
 
 const NewsLatter = () => {
   return (
-    <div className='newsletter'>
+    <div className='newslatter'>
         <h1>Get Exclusive offers on your Email</h1>
         <p>Subscribe to our newletter and stay updated</p>
         <div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import ShopContextProvider from './Context/ShopContext';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <ShopContextProvider>
+    <App />
+  </ShopContextProvider>
 );
