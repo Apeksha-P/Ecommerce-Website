b7b5e154072bb4a6a8bb97578b961af5a19ee85b
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-05-30 07:26:51 +0530

    Add description box

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div classname='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Welcome to our eCommerce website, your ultimate 
+            destination for all your shopping needs. We offer 
+            a wide range of products, from the latest tech 
+            gadgets and stylish fashion items to essential home 
+            goods and unique gifts. Our user-friendly platform 
+            ensures a seamless shopping experience with easy 
+            navigation, secure payment options, and fast shipping. 
+            Whether you're looking for everyday essentials or something 
+            special, our extensive catalog is designed to meet your 
+            every need. Customer satisfaction is our top priority, and 
+            our dedicated support team is always here to assist you. 
+            Shop with confidence and convenience on our eCommerce site, 
+            where quality meets affordability.</p>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
