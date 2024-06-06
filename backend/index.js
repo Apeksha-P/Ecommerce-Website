178a5a358d1afd0aba57e3b480abbd9a600fc17f
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-06-06 12:49:44 +0530

    database create

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,99 @@
 const port = 4000;
 const express = require("express");
 const app = express();
-const mongoose = require("mongooes");
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
 
-app.arguments(express.json());
-app.arguments(cors());
+app.use(express.json());
+app.use(cors());
  // Databasse connect with mongoDB
- mongoose.connect("mongodb+srv://apeksha:apeksha@123@cluster0.mwcfvvi.mongodb.net/e-commerce")
+ mongoose.connect("mongodb+srv://newproject:apeksha123@cluster0.wyp1wi5.mongodb.net/")
 
  //API creations
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+//Image storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(red,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage:storage})
+
+//Cretae upload endpoint for image
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success: 1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//Schema for creating products
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required: true,
+    },
+    name :{
+        type: String,
+        required: true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        default:true,
+    }
+})
+app.post('/addproduct',async(req,res)=>{
+    const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    })
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on port "+port)
+    }
+    else{
+        console.log("Error "+error)
+    }
+ })
