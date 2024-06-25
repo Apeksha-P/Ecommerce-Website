4f52a0bd3b3533748f0b742c142c025d270a38fb
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-06-25 17:57:45 +0530

    Add enpoints

--- backend/index.js
+++ backend/index.js
@@ -190,6 +190,43 @@ app.post('/login',async (req,res)=>{
     }
 })
 
+//creating endpoint for newcollection data
+app.get('/newcollections',async(req,res)=>{
+    let products = await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log("NewCollection Fetched");
+    res.send(newcollection);
+})
+
+//create endpoint for popular in women section
+app.get('/popularinwomen',async(req,res)=>{
+    let products = await Product.find({category:"women"})
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women Fetch");
+    res.send(popular_in_women);
+})
+//creating middelware to fetch user
+const fetchUser = async (req,res,next)=>{
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({errors:"Please authenticate using valid token"})
+    }
+    else{
+        try{
+            const data = jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        }catch(error){
+            res.status(401).send({errors:'please authenticate using a valid token'})
+        }
+    }
+}
+
+//creating endpont for adding products in cartdata
+app.post('/addtocart',fetchUser,async(req,res)=>{
+    console.log(req.body,req.user);
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on port "+port)
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,19 +1,36 @@
-import React, {createContext,useState} from 'react';
-import all_product from "../Components/Assets/all_product";
+import React, {createContext,useEffect,useState} from 'react';
 
 export const ShopContext = createContext(null);
 const getDfaultCart =()=>{
     let cart ={};
-    for (let index = 0; index < all_product.length+1; index++) {
+    for (let index = 0; index < 300+1; index++) {
         cart[index] = 0;
     }
     return cart;
 }
 const ShopContextProvider = (props)=>{
+    const [all_product,setall_Product]=useState([]);
     const [cartItems,setCartItems] = useState(getDfaultCart());
+    useEffect(()=>{
+        fetch('http://localhost:4000/allproducts')
+        .then((response)=>response.json())
+        .then((data)=>setall_Product(data))
+    },[])
     const addToCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        console.log(cartItems);
+        if(localStorage.getItem('auth-token')){
+            fetch('http://localhost:4000/addtocart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({"itemId":itemId}),
+            })
+            .then((response)=>response.json())
+            .then((data)=>console.log(data));
+        }
     }
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
