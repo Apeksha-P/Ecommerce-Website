253b72099af716a0b125b1b8741ac3663e6a29f6
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-06-25 16:19:00 +0530

    Create Signup funtion

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -3,20 +3,50 @@ import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   const [state,setState] =  useState("Sign Up");
+  const [formData,setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+  const changeHandler = (e) =>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+  const login = async ()=>{
+    console.log("Login function Executed",formData);
+  }
+  const signup = async ()=>{
+    console.log("Sign Up Function Executed",formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((response)=>response.json().then((data)=>responseData=data))
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
+    }
+  }
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state==="Sign Up"?<input type="text" placeholder='Your Name' />:<></>}
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} autoComplete='username' type="text" placeholder='Your Name' />:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} autoComplete='email' type="email" placeholder='Email Address' />
+          <input name='password' value={formData.password} onChange={changeHandler} autoComplete='current-password' type="password" placeholder='Password' />
         </div>
-        <button>Continue</button>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
         {state==="Sign Up"?<p className='loginsignup-login'>Already have an accunt? <span onClick={()=>{setState("Login")}}>Login here</span></p>:
         <p className='loginsignup-login'>Create an accunt? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
         <div className="loginsignup-agree">
-          <input type="checkbox" name="" id="" />
+        <input id="agree" type="checkbox" name="agree" />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
         </div>
       </div>
